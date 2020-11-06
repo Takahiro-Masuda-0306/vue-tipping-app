d9f7812311aeff991f9a31594d886461b34dd119
Takahiro-Masuda-0306 <taka.baseball.0306@gmail.com>
2020-11-06 15:24:33 +0900

    Modified: store.js Home.vue 課題4-2の「ログイン機能」にて、store.jsのデータ構造を修正しました。
    stateのuserの値をオブジェクトにすると同時に、レビュー内容にしたがって、元々のuserのstate値を保持できるようにしました。

--- vue-tipping-app/src/store.js
+++ vue-tipping-app/src/store.js
@@ -1,39 +1,44 @@
-import Vue from 'vue';
-import Vuex from 'vuex';
-import firebase from 'firebase';
-import router from './router';
-import 'firebase/auth';
+import Vue from "vue";
+import Vuex from "vuex";
+import firebase from "firebase";
+import router from "./router";
+import "firebase/auth";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
     user: {},
-    other_user_balance: 0,
-    users: [],
+    // other_user_balance: 0,
+    // users: [],
     errors: [],
   },
   getters: {
-    user: state => state.user,
-    other_user_balance: state => state.other_user_balance,
-    errors: state => state.errors,
-    users: state => state.users,
+    user: (state) => state.user,
+    // other_user_balance: (state) => state.other_user_balance,
+    errors: (state) => state.errors,
+    // users: (state) => state.users,
   },
   mutations: {
     setUser(state, user) {
-      state.user = {...user}
+      state.user = { ...state.user, ...user };
     },
-    setOtherUserBalance(state, other_user_balance) {
-      state.other_user_balance = other_user_balance;
-    },
-    setUsers(state, key, user) {
-      if(key !== user.id ) {
-        state.users.push(user);
-      }
-    },
+    // setOtherUserBalance(state, other_user_balance) {
+    //   state.other_user_balance = other_user_balance;
+    // },
+    // setUsers(state, user, user_id) {
+    //   if (user.key !== user_id) {
+    //     state.users.push(user);
+    //   }
+    // },
     setErrors(state, error) {
       state.errors.push(error);
     },
+    // resetState(state) {
+    //   state.user = {}
+    //   state.users = []
+    //   state.errors = []
+    // }
   },
   actions: {
     registerUser(context) {
@@ -47,7 +52,7 @@ export default new Vuex.Store({
           const user = response.user;
           firebase
             .database()
-            .ref('users')
+            .ref("users")
             .child(user.uid)
             .set({
               id: user.uid,
@@ -56,20 +61,27 @@ export default new Vuex.Store({
               balance: 500,
             })
             .then(() => {
-              router.push('/');
+              const userInfo = {
+                id: user.uid,
+                email: user.email,
+                name: context.getters.user.name,
+                balance: 500,
+              };
+              context.commit('setUser', userInfo);
+              router.push("/");
             })
             .catch((error) => {
               console.log(error);
             });
         })
         .catch((error) => {
-          if (error.code === 'auth/email-already-in-use') {
+          if (error.code === "auth/email-already-in-use") {
             context.getters.errors.push(
-              'このメールアドレスはすでに使われています。'
+              "このメールアドレスはすでに使われています。"
             );
           } else {
             context.getters.errors.push(
-              '入力されたメールアドレスかパスワードに問題があります。'
+              "入力されたメールアドレスかパスワードに問題があります。"
             );
           }
         });
@@ -77,49 +89,55 @@ export default new Vuex.Store({
     signIn(context) {
       firebase
         .auth()
-        .signInWithEmailAndPassword(context.getters.user.email, context.getters.user.password)
+        .signInWithEmailAndPassword(
+          context.getters.user.email,
+          context.getters.user.password
+        )
         .then((response) => {
           const user = response.user;
           firebase
             .database()
-            .ref('users')
+            .ref("users")
             .child(user.uid)
-            .on('value', snapshot => {
+            .on("value", (snapshot) => {
               const snapshotValue = {
                 id: snapshot.val().id,
                 name: snapshot.val().name,
                 email: snapshot.val().email,
                 balance: snapshot.val().balance,
               };
-              context.commit('setUser', snapshotValue);
+              context.commit("setUser", snapshotValue);
             });
 
-          firebase
-            .database()
-            .ref('users')
-            .on('value', snapshot => {
-              context.commit('setUsers', snapshot.key, snapshot.val());
-            });
-
-          router.push('/');
+          router.push("/");
         })
         .catch(() => {
-          context.commit('setErrors', 'メールアドレスかパスワードに誤りがあります。');
+          context.commit(
+            "setErrors",
+            "メールアドレスかパスワードに誤りがあります。"
+          );
         });
-      this.password = '';
+      this.password = "";
     },
-    // signOut() {
+    // showUsers(context) {
     //   firebase
-    //     .auth()
-    //     .signOut();
+    //     .database()
+    //     .ref("users")
+    //     .on("child_added", (snapshot) => {
+    //       context.commit("setUsers", snapshot, context.getters.user.id);
+    //     });
+    // },
+    // signOut(context) {
+    //   firebase.auth().signOut();
+    //   context.commit('resetState');
     // },
     // setOtherUserBalance(context, user_id) {
     //   firebase
-    //     .auth('users')
+    //     .auth("users")
     //     .ref(user_id)
-    //     .on('value', snapshot => {
-    //       context.commit('setOtherUserBalance', snapshot.val().balance);
+    //     .on("child_added", (snapshot) => {
+    //       context.commit("setOtherUserBalance", snapshot.val().balance);
     //     });
-    // }
+    // },
   },
 });
--- vue-tipping-app/src/views/Home.vue
+++ vue-tipping-app/src/views/Home.vue
@@ -7,14 +7,15 @@
     </div>
     <div>
       <h2 class="text-center mb-4 mt-4">ユーザー一覧</h2>
+      {{user}}
     </div>
-    <div  v-for="user in users" :key="user.id"> 
+    <div class="mb-2" v-for="(other_user, index) in users" :key="index"> 
       <div class="row text-center">
         <div class="col-sm-6">
-          <p>{{ user.name }}</p>
+          <p>{{ other_user.name }}</p>
         </div>
         <div class="col-sm-6">
-          <button class="btn btn-info" @click="confirmOtherUserBalance(user.id)">Walletを見る</button>
+          <button class="btn btn-info mr-2" @click="confirmOtherUserBalance(user.id)">Walletを見る</button>
           <button class="btn btn-info">送る</button>
         </div>
       </div>
@@ -26,31 +27,34 @@
 </template>
 
 <script>
-// export default {
-//   data() {
-//     return {
-//       confirm_balance: false,
-//     }
-//   },
-//   computed: {
-//     user() {
-//       return this.$store.getters.user;
-//     },
-//     other_user_balance() {
-//       return this.$store.getters.other_user_balance;
-//     },
-//     users() {
-//       return this.$store.getters.users;
-//     }
-//   },
-//   methods: {
-//     signOut() {
-//       this.$store.dispatch('signOut');
-//     },
-//     confirmOtherUserBalance(user_id) {
-//       this.confirm_balance = true;
-//       this.$store.dispatch('setOtherUserBalance', user_id);
-//     }
-//   }
-// }
+export default {
+  // data() {
+  //   return {
+  //     confirm_balance: false,
+  //   }
+  // },
+  // mounted() {
+  //   this.$store.dispatch('showUsers');
+  // },
+  // computed: {
+  //   other_user_balance() {
+  //     return this.$store.getters.other_user_balance;
+  //   },
+  //   user() {
+  //     return this.$store.getters.user;
+  //   },
+  //   users() {
+  //     return this.$store.getters.users;
+  //   }
+  // },
+  // methods: {
+    // signOut() {
+    //   this.$store.dispatch('signOut');
+    // },
+    // confirmOtherUserBalance(user_id) {
+    //   this.confirm_balance = true;
+    //   this.$store.dispatch('setOtherUserBalance', user_id);
+    // }
+  // }
+}
 </script>
