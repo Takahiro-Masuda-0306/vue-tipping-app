a6bf16bdbb3a4ed418dbf5f3685c3aa4d45ef45b
Takahiro-Masuda-0306 <taka.baseball.0306@gmail.com>
2020-11-08 15:05:22 +0900

    Modified: Home.vue 課題4の「ダッシュボード機能」を実装しました。
    ●具体的な内容
    ログイン後はダッシュボードへ遷移
    ダッシュボードでは、ログインしているユーザ名が表示される
    ダッシュボードでは、自身のウォレット（残高）が表示

--- vue-tipping-app/src/views/Home.vue
+++ vue-tipping-app/src/views/Home.vue
@@ -1,3 +1,18 @@
 <template>
-  <div class="container"></div>
+  <div class="container">
+    <div class="d-flex justify-content-between">
+      <p>{{ user.name }}さんようこそ!</p>
+      <p>残高：{{ user.balance }}</p>
+    </div>
+  </div>
 </template>
+
+<script>
+export default {
+  computed: {
+    user() {
+      return this.$store.getters.user;
+    },
+  },
+};
+</script>
